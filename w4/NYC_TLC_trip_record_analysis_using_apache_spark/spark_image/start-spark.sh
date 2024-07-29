5b2b4d32cc56e373ad46cfe86c49efa151d95afe
dlawork9888 <dlawork9888@gmail.com>
2024-07-29 21:55:46 +0900

    아직다안적음

--- w4/NYC_TLC_trip_record_analysis_using_apache_spark/spark_image/start-spark.sh
+++ w4/NYC_TLC_trip_record_analysis_using_apache_spark/spark_image/start-spark.sh
@@ -4,8 +4,15 @@
 
 # master -> start-master.sh / worker
 if [ "$SPARK_MASTER" = "true" ]; then
+    if [ ! -d "/opt/hadoop/data/namenode/current" ]; then
+        hdfs namenode -format -force -nonInteractive
+        sudo chmod 777 /opt/hadoop/data/namenode/
+    fi
+    hdfs --daemon start namenode
+    hdfs --daemon start secondarynamenode
     $SPARK_HOME/sbin/start-master.sh
 else
+    hdfs --daemon start datanode    
     $SPARK_HOME/sbin/start-worker.sh spark://spark-master:7077
 fi
 
---
+++ w4/NYC_TLC_trip_record_analysis_using_apache_spark/start-hadoop-spark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# NYC_TLC_data 폴더안의 모든 파일을 컨테이너 로컬 파일 시스템(/temp)으로 이동
+docker cp NYC_TLC_data/ spark-master:/temp
+
+# hdfs에 NYC_TLC_data 디렉토리 생성
+docker exec -it spark-master hdfs dfs -mkdir /NYC_TLC_data
+
+# 폴더 확인
+docker exec -it spark-master hdfs dfs -ls /
+
+# 로컬파일시스템의 /temp에 있는 모든 파일을 hdfs로 업로드 
+docker exec -it spark-master hdfs dfs -put /temp /NYC_TLC_data
+# => temp폴더 전체가 올라가서 NYC_TLC_data/temp/개별파일들 <- 요래됐음;;;
+
